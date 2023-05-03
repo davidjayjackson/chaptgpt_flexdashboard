d095006aa542ab546ec197d2c6b675293506414a
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-05-03 18:03:32 -0400

    app_1.R is the current version of the dashboard.

---
+++ hr_dashboard/app_1.R
@@ -0,0 +1,95 @@
+library(shiny)
+library(tibble)
+library(tidyverse)
+
+# Create the employee table
+employee <- tribble(
+  ~department_id, ~employee_id, ~name, ~hire_date, ~gender, ~salary,
+  1, 101, "Alice", "2015-01-01", "Female", 50000,
+  1, 102, "Bob", "2016-02-01", "Male", 60000,
+  1, 103, "Charlie", "2017-03-01", "Male", 70000,
+  2, 201, "Dave", "2018-04-01", "Male", 80000,
+  2, 202, "Eve", "2019-05-01", "Female", 90000
+)
+
+# Create the department table
+department <- tribble(
+  ~department_id, ~department_name,
+  1, "Sales",
+  2, "Engineering"
+)
+
+# Define UI
+ui <- fluidPage(
+  titlePanel("HR Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "department",
+                  label = "Select department:",
+                  choices = c("All", as.character(department$department_name)),
+                  selected = "All")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Employees", 
+                 tableOutput("employee_table")),
+        tabPanel("Department Summary",
+                 plotOutput("pie_chart")),
+        tabPanel("Salary Summary",
+                 plotOutput("bar_chart"))
+      )
+    )
+  )
+)
+
+# Define server
+server <- function(input, output) {
+  
+  # Convert department_id in employee table to department_name
+  employee_data <- reactive({
+    employee %>%
+      left_join(department, by = "department_id") %>%
+      mutate(department_name = if_else(is.na(department_name), "All", department_name))
+  })
+  
+  # Filter employee data based on selected department
+  department_data <- reactive({
+    if (input$department == "All") {
+      employee_data()
+    } else {
+      employee_data() %>%
+        filter(department_name == input$department)
+    }
+  })
+  
+  # Output table of employees in selected department
+  output$employee_table <- renderTable({
+    department_data()
+  })
+  
+  # Output pie chart of total salary by department
+  output$pie_chart <- renderPlot({
+    employee_data() %>%
+      group_by(department_name) %>%
+      summarise(total_salary = sum(salary)) %>%
+      ggplot(aes(x = "", y = total_salary, fill = department_name)) +
+      geom_bar(stat = "identity", width = 1) +
+      coord_polar("y", start=0) +
+      labs(title = "Salary Summary", fill = "Department") +
+      theme_void() +
+      theme(legend.position = "bottom")
+  })
+  
+  # Output bar chart of employee salaries in selected department
+  output$bar_chart <- renderPlot({
+    department_data() %>%
+      ggplot(aes(x = name, y = salary, fill = department_name)) +
+      geom_col() +
+      labs(title = "Employee Salaries by Department", x = "Employee Name", y = "Salary", fill = "Department") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      theme(legend.position = "right")
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
--- hr_dashboard/app_2.R
+++ hr_dashboard/app_2.R
@@ -31,16 +31,12 @@ ui <- fluidPage(
     ),
     mainPanel(
       tabsetPanel(
-        type = "tabs",
-        tabPanel(
-          "Employees",
-          tableOutput("employee_table"),
-          plotOutput("salary_by_department_plot")
-        ),
-        tabPanel(
-          "Salaries",
-          plotOutput("salary_histogram")
-        )
+        tabPanel("Employees", 
+                 tableOutput("employee_table")),
+        tabPanel("Department Summary",
+                 plotOutput("pie_chart")),
+        tabPanel("Salary Summary",
+                 plotOutput("bar_chart"))
       )
     )
   )
@@ -55,7 +51,7 @@ server <- function(input, output) {
       employee
     } else {
       employee %>%
-        filter(department_id == department[department$department_name == input$department, "department_id"])
+        filter(department_id == department$department_id[department$department_name == input$department])
     }
   })
   
@@ -64,24 +60,25 @@ server <- function(input, output) {
     department_data()
   })
   
-  # Output pie plot of total salary by department
-  output$salary_by_department_plot <- renderPlot({
+  # Output pie chart of total salary by department
+  output$pie_chart <- renderPlot({
     department_data() %>%
       group_by(department_id) %>%
       summarize(total_salary = sum(salary)) %>%
       inner_join(department, by = "department_id") %>%
       ggplot(aes(x = department_name, y = total_salary, fill = department_name)) +
       geom_bar(stat = "identity") +
-      ggtitle("Total Salary by Department") +
-      theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
+      labs(title = "Total Salary by Department", 
+           x = "Department", y = "Total Salary")
   })
   
-  # Output histogram of employee salaries
-  output$salary_histogram <- renderPlot({
-    ggplot(department_data(), aes(x = salary)) +
-      geom_histogram(binwidth = 10000) +
-      ggtitle("Employee Salary Distribution") +
-      theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
+  # Output bar chart of employee salaries in selected department
+  output$bar_chart <- renderPlot({
+    department_data() %>%
+      ggplot(aes(x = name, y = salary)) +
+      geom_bar(stat = "identity") +
+      labs(title = "Employee Salaries",
+           x = "Employee Name", y = "Salary")
   })
 }
 
