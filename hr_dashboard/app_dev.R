89b433c75d213baa3b1db03bf1db83fec0bc910c
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-05-15 16:27:57 -0400

    Added more emplpyees and marketing department and a plot of hires by year.

--- hr_dashboard/app_dev.R
+++ hr_dashboard/app_dev.R
@@ -8,17 +8,22 @@ employee <- tribble(
   1, 101, "Alice", "2015-01-01", "Female", 50000,
   1, 102, "Bob", "2016-02-01", "Male", 60000,
   1, 103, "Charlie", "2017-03-01", "Male", 70000,
-  2, 201, "Dave", "2018-04-01", "Male", 80000,
-  2, 202, "Eve", "2019-05-01", "Female", 90000
+  2, 201, "Dave", "2017-04-01", "Male", 80000,
+  2, 202, "Eve", "2017-05-01", "Female", 90000,
+  1, 104, "Frank", "2020-06-01", "Male", 55000,
+  3, 203, "Grace", "2020-07-01", "Female", 65000,
+  3, 204, "Henry", "2022-08-01", "Male", 75000,
+  3, 105, "Ivy", "2022-09-01", "Female", 85000,
+  1, 106, "Jack", "2023-10-01", "Male", 95000
 )
 
 # Create the department table
 department <- tribble(
   ~department_id, ~department_name,
   1, "Sales",
-  2, "Engineering"
+  2, "Engineering",
+  3, "Marketing"
 )
-
 # Define UI
 ui <- fluidPage(
   titlePanel("HR Dashboard"),
@@ -40,7 +45,9 @@ ui <- fluidPage(
         tabPanel("Department Summary",
                  plotOutput("pie_chart")),
         tabPanel("Salary Summary",
-                 plotOutput("bar_chart"))
+                 plotOutput("bar_chart")),
+        tabPanel("Hiring Summary",
+                 plotOutput("line_chart"))
       )
     )
   )
@@ -99,7 +106,19 @@ server <- function(input, output) {
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       theme(legend.position = "right")
   })
+  
+  # Output line chart of total employees by year
+  output$line_chart <- renderPlot({
+    department_data() %>%
+      group_by(year = year(hire_date)) %>%
+      summarise(total_employees = n()) %>%
+      ggplot(aes(x = year, y = total_employees)) +
+      geom_line() +
+      labs(title = "Hiring Summary", x = "Year", y = "Total Employees") +
+      theme_minimal()
+  })
 }
 
 # Run the app
 shinyApp(ui = ui, server = server)
+
